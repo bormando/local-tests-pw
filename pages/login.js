8e4d8234c88855d62b0e4612894e68fb61d0d56a
Dmitrii Bormotov <squier7@gmail.com>
2023-09-16 06:33:32 +0300

    Update selectors path

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,17 @@
-export default class LoginPage {
-  constructor(page) {
-    this.page = page
+import Page from './page'
 
-    this.inputEmail = page.locator('#normal_login_email')
-    this.inputPassword = page.locator('#normal_login_password')
-    this.buttonSubmit = page.locator('button[type="submit"]')
-    this.toast = page.locator('.ant-notification-notice-message')
+export default class LoginPage extends Page {
+  constructor(page) {
+    super(page)
+
+    this.input = {
+      email: page.locator('#normal_login_email'),
+      password: page.locator('#normal_login_password'),
+    }
+
+    this.button = {
+      submit: page.locator('button[type="submit"]'),
+    }
   }
 
   async open() {
@@ -13,8 +19,8 @@ export default class LoginPage {
   }
 
   async logIn(email, password) {
-    await this.inputEmail.fill(email)
-    await this.inputPassword.fill(password)
-    await this.buttonSubmit.click()
+    await this.input.email.fill(email)
+    await this.input.password.fill(password)
+    await this.button.submit.click()
   }
 }
